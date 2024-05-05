02a2b8cf0cc21d7988ca48783cde0b82e4879240
Sanket-A <be2018se600@gces.edu.np>
2024-05-05 22:32:29 +0545

    [frontend and resopnsiceness]

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import "./Navbar.css"
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
+import { useContext } from 'react'
+import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/nav_dropdown.png'
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop")
+    const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
   return (
     <div className='navbar'>
     <div className='nav-logo'>
     <img src={logo} alt="" />
     <p>DIVA</p></div>
-    <ul className='nav-menu'>
+    <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt=''/>
+    <ul  ref={menuRef} className='nav-menu'>
         <li onClick={()=>{setMenu("shop")}}><Link to= "/" style={{ textDecoration: 'none'}}>Shop</Link>{menu==="shop"? <hr/>:<></>}</li>
         <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none'}} to= "/mens">Men</Link>{menu==="mens"? <hr/>:<></>}</li>
         <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none'}} to= "/womens">Women</Link>{menu==="womens"? <hr/>:<></>}</li>
@@ -21,7 +32,7 @@ const Navbar = () => {
         <div className='nav-login-cart'>
            <Link to="/login">  <button>Login</button> </Link>
        <Link to= "/cart">   <img src={cart_icon} alt=" "/></Link>  
-    <div className='nav-cart-count'> </div>
+    <div className='nav-cart-count'>{getTotalCartItems()}</div>
 
             </div>
             </div>
--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from "../Assets/star_icon.png"
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from '../../Context/ShopContext';
 const ProductDisplay = (props) => {
     const {product} = props;
-  return (
+    const {addToCart} = useContext(ShopContext);
+  return ( 
    <div className="productdisplay">
     <div className="productdisplay-left">
         <div className="productdisplay-img-list">
@@ -44,7 +46,9 @@ A lightweight, usually knitted pullovershirt, close-fitting and with a round nec
                 <div>XXL</div>
             </div>
         </div>
-    <button> ADD TO CART</button>
+      
+    <button onClick={()=>{addToCart(product.id)}}>
+ ADD TO CART</button>
     <p className='productdisplay-right-category'><span>Category:</span>Women, T-Shirt, Crop Top</p>
     <p className='productdisplay-right-category'><span>Tags:</span>Modern, Latest</p>
 
